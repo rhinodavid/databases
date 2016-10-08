7383317a98febd871aa269474db866456448b477
rhinodavid <dawalsh@gmail.com>
2016-10-07 21:01:45 -0700

    Implement posting messages

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,7 +3,22 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    post: function (req, res) {
+
+      var message = {
+        username: req.body.username,
+        text: req.body.text,
+        roomname: req.body.roomname
+      };
+
+      models.messages.post(message, function(err, results) {
+        if (err) {
+          res.status(400).end('Error posting message.');
+        } else {
+          res.status(201).send({ id: results.insertId, createdAt: results.createdAt });
+        }
+      });
+    } // a function which handles posting a message to the database
   },
 
   users: {
--- server/models/index.js
+++ server/models/index.js
@@ -3,16 +3,38 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function(message, callback) {
+      // need user_id from username
+      module.exports.users.post(message.username, function(err, results) {
+
+        if (err) {
+          callback(err, null);
+        } else {
+          var userId = results.insertId;
+          var queryString = 'insert into messages (user_id, room, text) values (?, ?, ?)';
+          var queryArgs = [userId, message.roomname, message.text];
+
+          db.query(queryString, queryArgs, function(err, results) {
+            if (err) {
+              callback(err, null);
+            } else {
+              callback(null, results);
+            }
+          });          
+        }
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
     get: function () {},
-    post: function (username, callback) {
+    post: function(username, callback) {
       // atempt to insert to DB
-      var queryString = 'insert into users (name) values ("?")';
-      var queryArgs = [username];
+      var queryString = 'insert into users (name) values ("?") on \
+        duplicate key update id = last_insert_id(id), name="?"';
+
+      var queryArgs = [username, username];
 
       db.query(queryString, queryArgs, function(err, results) {
         if (err) {
@@ -21,7 +43,6 @@ module.exports = {
           callback(null, results);
         }
       });
-
     }
   },
 
